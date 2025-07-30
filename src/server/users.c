7d54b51c422d54939727c9430bfbdb88055d1eef
valerioisufi <66517412+valerioisufi@users.noreply.github.com>
2025-07-31 01:37:03 +0200

    Refactor: Migliorata la gestione di utenti e partite

--- src/server/users.c
+++ src/server/users.c
@@ -34,11 +34,15 @@ void init_lists() {
 int create_user(const char *username, int socket_fd) {
     User *new_user = (User *)malloc(sizeof(User));
     if (!new_user) return -1;
-    
-    new_user->username = strdup(username);
-    if (!new_user->username) {
-        free(new_user);
-        return -1;
+
+    if (username) {
+        new_user->username = strdup(username);
+        if (!new_user->username) {
+            free(new_user);
+            return -1;
+        }
+    } else {
+        new_user->username = NULL;
     }
     
     new_user->socket_fd = socket_fd;
@@ -74,6 +78,95 @@ void remove_user(unsigned int user_id) {
 }
 
 /**
+ * Aggiorna il file descriptor della socket associato a un utente.
+ * @param user_id ID dell'utente da aggiornare.
+ * @param socket_fd Nuovo file descriptor della socket.
+ * @return 0 se l'aggiornamento è andato a buon fine, -1 in caso di errore.
+ */
+int update_user_socket_fd(unsigned int user_id, int socket_fd) {
+    ListItem *node = get_node(user_id, users_list);
+    int success = -1;
+
+    pthread_mutex_lock(&node->mutex);
+    
+    if (node->ptr) {
+        User *user = (User *)node->ptr;
+        user->socket_fd = socket_fd;
+        success = 0;
+    }
+
+    pthread_mutex_unlock(&node->mutex);
+    return success;
+}
+
+/**
+ * Ottiene il file descriptor della socket associata a un utente.
+ * @param user_id ID dell'utente di cui ottenere il file descriptor.
+ * @return File descriptor della socket dell'utente, o -1 se l'utente non esiste.
+ */
+int get_user_socket_fd(unsigned int user_id) {
+    ListItem *node = get_node(user_id, users_list);
+    int socket_fd = -1;
+
+    pthread_mutex_lock(&node->mutex);
+    
+    if (node->ptr) {
+        User *user = (User *)node->ptr;
+        socket_fd = user->socket_fd;
+    }
+
+    pthread_mutex_unlock(&node->mutex);
+    return socket_fd;
+}
+
+/**
+ * Aggiorna il nome utente associato a un ID utente.
+ * @param user_id ID dell'utente da aggiornare.
+ * @param new_username Nuovo nome utente da assegnare.
+ * @return 0 se l'aggiornamento è andato a buon fine, -1 in caso di errore.
+ */
+int update_user_username(unsigned int user_id, const char *new_username) {
+    ListItem *node = get_node(user_id, users_list);
+    int success = -1;
+
+    pthread_mutex_lock(&node->mutex);
+    
+    if (node->ptr) {
+        User *user = (User *)node->ptr;
+        if(user->username) {
+            free(user->username);
+        }
+        user->username = strdup(new_username);
+        if (user->username) {
+            success = 0;
+        }
+    }
+
+    pthread_mutex_unlock(&node->mutex);
+    return success;
+}
+
+/**
+ * Ottiene il nome utente associato a un ID utente.
+ * @param user_id ID dell'utente di cui ottenere il nome.
+ * @return Nome dell'utente, o NULL se l'utente non esiste.
+ */
+char *get_username_by_id(unsigned int user_id) {
+    ListItem *node = get_node(user_id, users_list);
+    char *username = NULL;
+
+    pthread_mutex_lock(&node->mutex);
+
+    if (node->ptr) {
+        User *user = (User *)node->ptr;
+        username = strdup(user->username);
+    }
+
+    pthread_mutex_unlock(&node->mutex);
+    return username;
+}
+
+/**
  * Aggiorna l'ID della partita associata a un utente.
  * @param user_id ID dell'utente da aggiornare.
  * @param game_id Nuovo ID della partita, 0 se l'utente non è in una partita.
@@ -93,6 +186,26 @@ int update_user_game_id(unsigned int user_id, unsigned int game_id) {
 
     pthread_mutex_unlock(&node->mutex);
     return success;
+}
+
+/**
+ * Ottiene l'ID della partita associata a un utente.
+ * @param user_id ID dell'utente di cui ottenere l'ID della partita.
+ * @return ID della partita associata all'utente, o 0 se l'utente non è in una partita.
+ */
+int get_user_game_id(unsigned int user_id) {
+    ListItem *node = get_node(user_id, users_list);
+    int game_id = 0;
+
+    pthread_mutex_lock(&node->mutex);
+    
+    if (node->ptr) {
+        User *user = (User *)node->ptr;
+        game_id = user->game_id;
+    }
+
+    pthread_mutex_unlock(&node->mutex);
+    return game_id;
 }
 
 
@@ -115,8 +228,8 @@ int create_game(const char *game_name, unsigned int owner_id) {
     new_game->owner_id = owner_id;
     new_game->players_capacity = 8;
     new_game->players_count = 0;
-    new_game->players_ids = (unsigned int *)malloc(new_game->players_capacity * sizeof(unsigned int));
-    if (!new_game->players_ids) {
+    new_game->player_ids = (unsigned int *)malloc(new_game->players_capacity * sizeof(unsigned int));
+    if (!new_game->player_ids) {
         free(new_game->game_name);
         free(new_game);
         return -1;
@@ -143,7 +256,7 @@ void remove_game(unsigned int game_id) {
     Game *game_data = (Game *)node->ptr;
     if (game_data) {
         free(game_data->game_name);
-        free(game_data->players_ids);
+        free(game_data->player_ids);
         free(game_data);
         node->ptr = NULL;
     }
@@ -171,9 +284,9 @@ int add_player_to_game(unsigned int game_id, unsigned int player_id) {
         // Se l'array dei giocatori è pieno, raddoppia la sua capacità
         if (game->players_count >= game->players_capacity) {
             size_t new_capacity = game->players_capacity * 2;
-            unsigned int *new_players = (unsigned int *)realloc(game->players_ids, new_capacity * sizeof(unsigned int));
+            unsigned int *new_players = (unsigned int *)realloc(game->player_ids, new_capacity * sizeof(unsigned int));
             if (new_players) {
-                game->players_ids = new_players;
+                game->player_ids = new_players;
                 game->players_capacity = new_capacity;
             } else {
                 // realloc fallito, non si può aggiungere il giocatore
@@ -183,7 +296,7 @@ int add_player_to_game(unsigned int game_id, unsigned int player_id) {
         }
         
         // Aggiungi il giocatore
-        game->players_ids[game->players_count] = player_id;
+        game->player_ids[game->players_count] = player_id;
         game->players_count++;
         success = 0;
     }
@@ -201,9 +314,9 @@ int remove_player_from_game(unsigned int game_id, unsigned int player_id) {
     Game *game = (Game *)node->ptr;
     if (game) {
         for (unsigned int i = 0; i < game->players_count; i++) {
-            if (game->players_ids[i] == player_id) {
+            if (game->player_ids[i] == player_id) {
                 // Sposta l'ultimo giocatore nella posizione corrente
-                game->players_ids[i] = game->players_ids[game->players_count - 1];
+                game->player_ids[i] = game->player_ids[game->players_count - 1];
                 game->players_count--;
                 success = 0;
                 break;
--- src/server/users.h
+++ src/server/users.h
@@ -11,7 +11,7 @@ typedef struct {
     unsigned int game_id; // ID univoco della partita
     unsigned int owner_id; // ID dell'utente che ha creato la partita
 
-    unsigned int *players_ids;
+    unsigned int *player_ids;
     unsigned int players_capacity; // Capacità attuale dell'array dei giocatori
     unsigned int players_count; // Numero attuale di giocatori nella partita
 } Game;
@@ -20,7 +20,12 @@ void init_lists();
 
 int create_user(const char *username, int socket_fd);
 void remove_user(unsigned int user_id);
+int update_user_socket_fd(unsigned int user_id, int socket_fd);
+int get_user_socket_fd(unsigned int user_id);
+int update_user_username(unsigned int user_id, const char *username);
+char *get_username_by_id(unsigned int user_id);
 int update_user_game_id(unsigned int user_id, unsigned int game_id);
+unsigned int get_user_game_id(unsigned int user_id);
 
 int create_game(const char *game_name, unsigned int owner_id);
 void remove_game(unsigned int game_id);
